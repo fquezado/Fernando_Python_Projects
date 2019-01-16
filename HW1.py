73044aaa06600074dcf8a8e853b1fc1e963aab9b
fquezado <42972165+fquezado@users.noreply.github.com>
2019-01-15 21:59:43 -0500

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,7 @@
+user_input_celsius = float(input("Enter a Celsius temperature to convert to Fahrenheit: "))     #taking input in
+
+F = ((9/5) * user_input_celsius) + 32   #Converting Celsius input to fahrenheit
+
+F = round(F, 1)     #Rounding to one decimal place
+
+print("The Fahrenheit equivalent is: " + str(F))   #printing desired output
