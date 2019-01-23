9cf40875cac0ff032dae09ac02a41c316ee2a616
fquezado <42972165+fquezado@users.noreply.github.com>
2019-01-23 11:44:01 -0500

    Update Simple_Celsius_to_Fahrenheit_converter.py

--- Simple_Celsius_to_Fahrenheit_converter.py
+++ Simple_Celsius_to_Fahrenheit_converter.py
@@ -1,7 +1,7 @@
-user_input_celsius = float(input("Enter a Celsius temperature to convert to Fahrenheit: "))     #taking input in
+user_input_celsius = float(input("Enter a Celsius temperature to convert to Fahrenheit: "))   #taking user input in
 
 F = ((9/5) * user_input_celsius) + 32   #Converting Celsius input to fahrenheit
 
-F = round(F, 1)     #Rounding to one decimal place
+F = round(F, 1)  #Rounding to one decimal place
 
 print("The Fahrenheit equivalent is: " + str(F))   #printing desired output
